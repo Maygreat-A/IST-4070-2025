cb5d04ad442737dc299f19c59dd0fb570cc815cc
Margret A Wafula <margret.atemi.wafula@outlook.com>
2025-09-28 16:00:51 +0300

    Add files via upload
    The second lab on OOP;
    Relationship between classes and objects.
    The object "emma" is an instance of the class "Account"

---
+++ Account.java
@@ -0,0 +1,12 @@
+public class Account 
+{
+
+public  int accountNo;
+public String accountName;
+public double balance;
+
+public void deposit(double amount)
+{
+balance = balance + amount;
+}
+}
---
+++ CreateAccount.java
@@ -0,0 +1,13 @@
+public class CreateAccount {
+public static void main(String[] args) 
+{
+Account emmaAccount = new Account(); // creating an object 
+emmaAccount.accountNo = 1423;
+emmaAccount.accountName = "Emma";
+emmaAccount.balance = 500;
+
+System.out.println("A/c no: " + emmaAccount.accountNo +
+" A/c name: " + emmaAccount.accountName + " Balance: "
++ emmaAccount.balance);
+}
+}
